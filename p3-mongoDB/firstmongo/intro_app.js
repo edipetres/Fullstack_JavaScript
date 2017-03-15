b12f734898a0944500f2af1870cba8ce084bab30
eddie <eddmond@Edmonds-MacBook-Pro.local>
2017-03-15 20:22:18 +0100

    updated intro exercise

--- p3-mongoDB/firstmongo/intro_app.js
+++ p3-mongoDB/firstmongo/intro_app.js
@@ -4,23 +4,12 @@ var assert = require('assert');
 // Connection url
 var url = "mongodb://localhost:27017/classdemo";
 
-MongoClient.connect(url, function (err, db) {
-  assert.equal(null, err);
-  console.log("Conneceted successfully to the server!")
-  insertDocuments(db, function () {
-    findDocuments(db, function () {
-      db.close();
-    });
-  });
-
-});
-
 var findDocuments = function (db, callback) {
   // Get the documents collection
   var collection = db.collection('documents')
 
   // Find some documents
-  collection.find({'a':3}).toArray(function (err, docs) {
+  collection.find({ 'a': 3 }).toArray(function (err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs)
@@ -41,5 +30,28 @@ var insertDocuments = function (db, callback) {
     console.log("Inserted 3 documents into the collection");
     callback(result);
   });
-
 }
+
+var updateDocument = function (db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a: 2 }, { $set: { b: 1 } }, function (err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+};
+
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+  console.log("Conneceted successfully to the server!")
+  insertDocuments(db, function () {
+    updateDocument(db, function () {
+      findDocuments(db, function () {
+        db.close();
+      })
+    });
+  });
+});
