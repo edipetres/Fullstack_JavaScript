7dd9883a4d3fc5791b3fccf70f749c1537984202
Edmond <edipetres@gmail.com>
2017-03-20 19:14:57 +0100

    Set up Mocha and Chai tests

--- p3-mongoDB/MEAN-jokes/test/joke-tester.js
+++ p3-mongoDB/MEAN-jokes/test/joke-tester.js
@@ -1,6 +1,37 @@
+var dbfacade = require('../model/jokes')
+var connection = require('../db/db')
+var connection_string = "mongodb://localhost/test";
+var expect = require('chai').expect;
 
-describe("Jokes API", function() {
-    it("returns all jokes", function() {
-        
-    })
+
+
+
+describe("Server with API", function () {
+
+    before(function (done) {
+        connection.connect(connection_string, function (err, db) {
+            if (err) {
+                console.log(err)
+            };
+            done();
+        });
+    });
+
+    it("should connect to database", function () {
+        var db = connection.get();
+        expect(db).to.not.equal(undefined);
+    });
+
+    it("should return a joke without error", function () {
+        dbfacade.findJoke('58cff668ad6dbf60c14067ce', function (err, docs) {
+            expect(err).to.equal(undefined);
+            expect(docs.length).to.be.greaterThan(0);
+        });
+    });
+
 });
+
+
+
+
+
